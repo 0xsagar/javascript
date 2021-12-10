487601ac0a8ef4a4db1e1b0413f37cfa9053e447
0xsagar <sagar.19bct@cmr.edu.in>
2021-12-10 23:46:52 +0530

    mini project - landing page completed

--- ProjectPage/.next/static/chunks/webpack.js
+++ ProjectPage/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cda5dc2af7d48b4"; }
+/******/ 		__webpack_require__.h = function() { return "b80a0ec59640cbfb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
